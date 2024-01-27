a7b2ddb2927b4b99335cd8fc5f77a1ad7e8bda63
AdiyaMitra <144686225+AdiyaMitra@users.noreply.github.com>
2024-01-27 13:35:23 +0000

    variable1 commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345;                //to declare constant can't be changed
+let accounEmail = `abcd@gmail.com`;     //
+var accountPassword = `54321`;          //
+accountCity = `Kolkata`;                //
+
+//  accountId = 2;      we can't write this beacuse of accountID is constant
+//  console.log(accountId);     if we print this it'll throw an error
+
+console.log(accountId);
+console.log(accounEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.log(accountId);
+accounEmail = `xyz@yahoo.com`;
+accountPassword = `98765`;
+accountCity = `Mumbai`;
+
+console.log(accounEmail);
+console.log(accountPassword);
+console.log(accountCity);

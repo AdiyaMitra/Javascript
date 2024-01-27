a658b93e22e7d0518bde74ce68e39d161a4151d1
AdiyaMitra <144686225+AdiyaMitra@users.noreply.github.com>
2024-01-27 13:45:32 +0000

    variable2 commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 12345;                //to declare constant can't be changed
-let accounEmail = `abcd@gmail.com`;     //
-var accountPassword = `54321`;          //
-accountCity = `Kolkata`;                //
+let accounEmail = `abcd@gmail.com`;     // let is block scoped
+var accountPassword = `54321`;          // but var is function scoped, and we don't prffer to use var because of confusion of accessibility
+accountCity = `Kolkata`;                // javascript allows to make variables without declaring  VAR or LET but this is no a good way to write code
 
 //  accountId = 2;      we can't write this beacuse of accountID is constant
 //  console.log(accountId);     if we print this it'll throw an error
@@ -19,3 +19,5 @@ accountCity = `Mumbai`;
 console.log(accounEmail);
 console.log(accountPassword);
 console.log(accountCity);
+
+console.table([accountId, accounEmail, accountPassword, accountCity])       //console.table prints data in a tabular format
